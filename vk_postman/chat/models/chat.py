b92d24062ef932cae4c61a47aadf75084d8e7d60
aq1 <inmosh@protonmail.ch>
2017-07-24 20:34:50 +0300

    Migration. Add telegram_active and vk_active fields

--- vk_postman/chat/models/chat.py
+++ vk_postman/chat/models/chat.py
@@ -7,3 +7,5 @@ class Chat(models.Model):
     vk_user = models.ForeignKey(VkUser)
     telegram_user = models.ForeignKey(TelegramUser)
     is_active = models.BooleanField(default=True, blank=True)
+    telegram_active = models.BooleanField(default=True, blank=True)
+    vk_active = models.BooleanField(default=True, blank=True)
