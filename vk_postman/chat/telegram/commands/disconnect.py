e01b0334e6632258ed366e4d09a03012de94452a
aq1 <inmosh@protonmail.ch>
2017-07-24 21:05:58 +0300

    Fix disconnect command

--- vk_postman/chat/telegram/commands/disconnect.py
+++ vk_postman/chat/telegram/commands/disconnect.py
@@ -12,12 +12,23 @@ class Disconnect(CommandBase):
     def _execute(cls, telegram_user_id, *args):
         telegram_user, _ = TelegramUser.objects.get_or_create(id=telegram_user_id)
 
-        chats = Chat.objects.filter(
-            telegram_user=telegram_user,
-            telegram_active=True,
-            vk_active=True,
-        ).update(telegram_active=False, vk_active=False)
-        if not chats:
+        try:
+            chat = Chat.objects.get(
+                telegram_user=telegram_user,
+                telegram_active=True,
+                vk_active=True,
+            )
+        except Chat.DoesNotExist:
             return True, 'You are not connected to any vk user'
+        except Chat.MultipleObjectsReturned:
+            Chat.objects.filter(telegram_user=telegram_user_id).update(
+                telegram_active=False,
+                vk_active=False,
+            )
+            return True, 'Disconnected from all chats.'
+
+        chat.telegram_active = False
+        chat.vk_active = False
+        chat.save()
 
         return True, cls._SUCCESS_MSG.format(vk_user=cls._get_vk_user_url(chat.vk_user_id))
