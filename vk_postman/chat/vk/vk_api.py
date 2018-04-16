0f8c0b45a2b1f7d15b938dff362828fdd109f0ff
aq1 <aq1@aq1.ru>
2018-04-16 16:41:48 +0300

    Add verstion to vk api requests

--- vk_postman/chat/vk/vk_api.py
+++ vk_postman/chat/vk/vk_api.py
@@ -11,4 +11,5 @@ def call(method, api_method, data=None, params=None):
     data = data or {}
     params = params or {}
     params['access_token'] = settings.VK_TOKEN
+    params['v'] = settings.VK_API_VERSION
     return r(URL.format(api_method), data=data, params=params)
