1e6fe4c84b22d802f71231bb3af7f3bb2e94af72
aq1 <aq1@aq1.ru>
2018-04-16 16:20:27 +0300

    Change settings again

--- vk_postman/settings/settings.py
+++ vk_postman/settings/settings.py
@@ -106,10 +106,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
