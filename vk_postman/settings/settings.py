df67a4e8bd40252c5424dc734be8c7d7837f3290
aq1 <aq1@aq1.ru>
2018-04-16 16:28:08 +0300

    Change settings again

--- vk_postman/settings/settings.py
+++ vk_postman/settings/settings.py
@@ -112,7 +112,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 
 EMAIL_HOST = 'smtp.yandex.ru'
