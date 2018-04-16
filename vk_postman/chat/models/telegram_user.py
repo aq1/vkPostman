a439b63a2e38cccb6d43441615f61d9e6a9dbaf4
aq1 <aq1@aq1.ru>
2018-04-16 16:16:13 +0300

    Fix admin, change static settings

--- vk_postman/chat/models/telegram_user.py
+++ vk_postman/chat/models/telegram_user.py
@@ -6,5 +6,5 @@ class TelegramUser(models.Model):
     first_name = models.CharField(max_length=255, default='', blank=True)
     last_name = models.CharField(max_length=255, default='', blank=True)
 
-    def __unicode__(self):
+    def __str__(self):
         return '{} {}'.format(self.last_name, self.first_name)
