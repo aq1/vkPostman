c10951d3bb4f31513e15a55ba8d0ffb72d8f12ee
aq1 <inmosh@protonmail.ch>
2018-08-21 00:04:35 +0300

    Delete local settings

--- settings/settings_local.py
+++
@@ -1,7 +0,0 @@
-MONGO_HOST = 'localhost'
-MONGO_PORT = 27017
-MONGO_DB = 'vk_postman'
-MONGO_USER = 'vk_postman_user'
-MONGO_PASSWORD = 'vk_postman_pass'
-
-TELEGRAM_TOKEN = '425846537:AAF0YHag86PTEq7WchEeLqsCp5Wg0UtAPTs'
